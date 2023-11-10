dfabab8e6fb6e8e673b087fa70840fb568a4c452
joser <jose.ruiztagle@mayor.cl>
2023-11-10 11:28:05 -0300

    LM

--- Regresión lineal.R
+++ Regresión lineal.R
@@ -1,3 +1,25 @@
 ####################
 # REGRESIÓN LINEAL #
 ####################
+
+# En su forma más simple, la regresión lineal establece una línea recta 
+# (llamada "línea de regresión") que mejor se ajusta a los datos datos, 
+# según el criterio de los mínimos cuadrados. Esta línea se representa con 
+# la ecuación y=b0+b1X1
+# donde y es la variable dependiente, 
+# x es la variable independiente, 
+# b1 es la pendiente de la línea, y 
+# y b0 es el intercepto.
+
+# El objetivo es minimizar la suma de los cuadrados de 
+# las diferencias entre los valores observados y los predichos por el modelo. 
+# Esto se hace ajustando la pendiente 
+# b1 y el intercepto b0 de la línea.
+
+# Los coeficientes indican la importancia de cada variable independiente en la 
+# predicción de la variable dependiente. Un coeficiente positivo sugiere una relación 
+# directa, mientras que un coeficiente negativo indica una relación inversa.
+
+# La regresión lineal se usa en muchas áreas, como economía, biología, 
+# ingeniería y ciencias sociales, para entender las relaciones entre variables 
+# y hacer predicciones.
